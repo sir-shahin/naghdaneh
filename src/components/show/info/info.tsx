ba62a9a9060d25787542d737aeacfe8abbb8cfc9
unknown <sir-shahin>
2025-08-06 12:51:46 +0330

    feat: info section completed

--- src/components/show/info/info.tsx
+++ src/components/show/info/info.tsx
@@ -10,17 +10,18 @@ import Typography from "@mui/material/Typography";
 import { RateBadge } from "@/components/common";
 import img80 from "@/assets/images/000/80.jpg";
 import { SVGUserOct } from "@/assets/images/user-oct";
+import { SVGUserProfile } from "@/assets/images/user-profile";
 
 export const Info = () => {
   return (
     <Container component={"section"}>
       <Grid container py={5}>
-        <Grid size={{ xs: 12, md: 3 }} display="flex" px={4}>
+        <Grid size={{ xs: 12, md: 4 }} display="flex" px={4}>
           <Box position="relative" flex={1} height="inherit" minHeight={360}>
-            <Image src={img80} alt="Info Image" fill style={{ objectFit: "contain" }} />
+            <Image src={img80} alt="Info Image" fill style={{ objectFit: "contain", borderRadius: 5 }} />
           </Box>
         </Grid>
-        <Grid size={{ xs: 12, md: 9 }} py={5}>
+        <Grid size={{ xs: 12, md: 8 }} py={5}>
           <Typography variant="h4" component="h1" gutterBottom>
             John Wick: Chapter 3 - Parabellum 2019
           </Typography>
@@ -32,30 +33,34 @@ export const Info = () => {
                 فیلم
               </Typography>
             </Stack>
-            {/* <Stack direction={"row"}>
-              <Typography>IMDB:</Typography>
-              <Typography px={1}>7.9/10</Typography>
-            </Stack> */}
           </Stack>
 
-          <Typography mb={3}>
+          <Typography mb={3} pl={{ md: 5 }}>
             جان ویک 3 یا جان ویک: بخش ۳ – پارابلوم، فیلمی اکشن مهیج و نئو-نوآر، به نویسندگی دریک کولستاد و کارگردانی چاد
             استاهلسکی است. این فیلم سومین قسمت از سری فیلم های جان ویک است. کیانو ریوز، هلی بری، ایان مک‌شین و لارنس
             فیشبرن به ایفای نقش می‌پردازند. آدمکش افسانه‌ای “جان ویک”؛ بعد از آنکه برای سرش جایزه ۱۴ میلیون دلاری
             گذاشته‌اند...
           </Typography>
 
-          <Stack direction={"row"} mb={3}>
-            <Typography fontSize={13}>ژانرها:</Typography>
-            <Typography px={1} fontSize={13}>
-              اکشن ، تخیلی
-            </Typography>
+          <Stack direction="row" mb={3} gap={4}>
+            <Stack direction={"row"}>
+              <Typography fontSize={13}>ژانرها:</Typography>
+              <Typography px={1} fontSize={13}>
+                اکشن ، تخیلی
+              </Typography>
+            </Stack>
+            <Stack direction={"row"}>
+              <Typography fontSize={13}>IMDB:</Typography>
+              <Typography px={1} fontSize={13}>
+                5/10
+              </Typography>
+            </Stack>
           </Stack>
 
-          <Stack direction={"row"} gap={3}>
+          <Stack direction={"row"} flexWrap={"wrap"} justifyContent={{ xs: "center", md: "start" }} gap={3}>
             <RateBadge title="کارشناس" rate={7} icon={<SVGUserOct />} />
-            <RateBadge title="کاربران" rate={7} icon={<SVGUserOct />} />
-            <RateBadge title="نقدانه" rate={7} icon={<SVGUserOct />} />
+            <RateBadge title="کاربران" rate={6} icon={<SVGUserProfile />} />
+            <RateBadge title="نقدانه" rate={3} icon={<SVGUserProfile />} />
           </Stack>
         </Grid>
       </Grid>
