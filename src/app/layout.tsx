d5cc7290a27495ee27a2ba7eeb3286233b6b688e
unknown <sir-shahin>
2025-08-12 11:09:31 +0330

    feat: loading added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 import "./globals.css";
 
+import NextTopLoader from "nextjs-toploader";
+
 import Header from "@/components/Header";
 import { Providers } from "@/providers";
 
@@ -15,10 +17,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const primaryColor = process.env.NEXT_PUBLIC_PRIMARY_COLOR;
+
   return (
     <html lang="fa" suppressHydrationWarning>
       <body suppressHydrationWarning>
         <Providers>
+          <NextTopLoader color={primaryColor} showSpinner={false} />
           <Header />
 
           {children}
